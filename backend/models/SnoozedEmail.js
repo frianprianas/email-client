339568490bc144583afd1d568ba0a5fbd92335ba
frianprianas <freeyhan87@hotmail.com>
2026-02-15 15:38:21 +0700

    Fix sequelize imports in models

--- backend/models/SnoozedEmail.js
+++ backend/models/SnoozedEmail.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { sequelize } = require('../config/database');
 
 const SnoozedEmail = sequelize.define('SnoozedEmail', {
     userId: {
